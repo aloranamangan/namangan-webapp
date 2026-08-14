69638254a3858875ee3c6dbf13e6e2d6ee4774e0
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-14 12:26:08 +0500

    Oq shisha panellar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v3';
+const CACHE = 'ni-v4';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'pano.js',
