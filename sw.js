d54a64ddc83b3bc78399a5598868633b06e6d2de
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-14 12:38:19 +0500

    Oq kristall marmar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v4';
+const CACHE = 'ni-v5';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'pano.js',
