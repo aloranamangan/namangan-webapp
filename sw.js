805d16f0d2d23f649a81fdc66c23db280fbb488b
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-14 12:21:27 +0500

    Pastki panel shisha

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v2';
+const CACHE = 'ni-v3';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'pano.js',
