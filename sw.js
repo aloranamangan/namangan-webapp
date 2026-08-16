3f94abd653052d692447584974a7adfbcfe31d44
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-16 23:17:06 +0500

    Logotip 60px

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v16';
+const CACHE = 'ni-v17';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'pano.js',
