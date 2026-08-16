f094366e868b8ffb53c217bcda7245986a3c6ece
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-16 12:50:04 +0500

    UYgram logotip va tepa panel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v11';
+const CACHE = 'ni-v12';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'pano.js',
