2eb6fa6552226f93d1aa4f0f37fde2687ad12de5
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-16 13:39:10 +0500

    Logotip kattaroq

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v14';
+const CACHE = 'ni-v15';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'pano.js',
