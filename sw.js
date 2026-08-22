60f78b5538dda2a0d06f193f511ab361803d6162
sbfcollectionuz-star <sbfcollectionuz@gmail.com>
2026-08-22 23:29:15 +0500

    Burger menyu va chiqish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ni-v34';
+const CACHE = 'ni-v35';
 const FILES = [
   'xaridor.html', 'sotuvchi.html', 'obhavo.html', 'valyuta.html',
   'style.css', 'weather.css', 'core.js', 'reg.js', 'dm.js', 'story.js', 'profil.js', 'intro.js', 'pano.js',
